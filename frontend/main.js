c86982e4e836529bfda8c8b95261408eee464514
John Marcial <johnmarloumarcial@gmail.com>
2022-02-03 07:34:18 +0800

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl ='https://getresumecounterr.azurewebsites.net/api/GetResumeCounter?code=swkrF26aHtkFuLLsEsyraTjVdflz8hjzuaoCaokoqFmB6ZOEBZWDEg=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
